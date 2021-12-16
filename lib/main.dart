4fdb63c0639da2765f06158cce7ec410e884de40
Sean Siders <sean.siders@icloud.com>
2021-12-15 23:05:02 -0800

    Test global api with GitHub Pages
    I decided to update app model information to users by using a Pogo Teams GitHub Page as a sort of file bucket. I made a few test calls in main, and things are working. The idea is that there will be a time stamp on the GitHub Page, indicating whether the client needs to update their data (stored locally via hive - sqlite).
    
    If they do need the update, the json information will be retrieved from the GitHub Page. This felt to me like the most reasonable free approach to providing updates to the app's model.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 // Flutter Imports
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // Package Imports
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 // Local Imports
 import 'pogo_teams_app.dart';
@@ -12,19 +15,42 @@ import 'data/teams_provider.dart';
 import 'data/masters/gamemaster.dart';
 import 'data/globals.dart' as globals;
 
+const String url = 'pogoteams.github.io';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Restrict view orientation to portrait only
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+  // Initialize the database for user data persistance
+  await Hive.initFlutter();
+  await Hive.openBox('teams'); // User data
+  Box gmBox = await Hive.openBox('gamemaster'); // Pokemon GO data
+
+  final client = http.Client();
+
+  try {
+    var response = await client.get(
+      Uri.https(url, '/timestamp.txt'),
+    );
+
+    final timestamp = jsonDecode(json.encode(response.body));
+    print(timestamp);
+
+    response = await client.get(
+      Uri.http(url, '/rankings/all/attackers/rankings-500.json'),
+    );
+
+    final gamemaster = jsonDecode(utf8.decode(response.bodyBytes));
+    print(gamemaster);
+  } finally {
+    client.close();
+  }
+
   // Global gamemaster reference setup
   // All Pokemon GO related data is in the gamemaster
   globals.gamemaster = await GameMaster.generateGameMaster();
-
-  // Initialize the database for user data persistance
-  await Hive.initFlutter();
-  await Hive.openBox('teams');
 
   runApp(
     ChangeNotifierProvider(
