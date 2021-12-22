67d2af0ddda211ac9657bb98570a3f9d22bddbf6
Sean Siders <sean.siders@icloud.com>
2021-12-21 17:25:53 -0800

    Updated app icons and removed unused code

--- lib/configs/size_config.dart
+++ lib/configs/size_config.dart
@@ -15,11 +15,6 @@ class SizeConfig {
   static late double blockSizeHorizontal;
   static late double blockSizeVertical;
 
-  static late double _safeAreaHorizontal;
-  static late double _safeAreaVertical;
-  static late double safeBlockHorizontal;
-  static late double safeBlockVertical;
-
   static late double h1;
   static late double h2;
   static late double h3;
@@ -31,13 +26,6 @@ class SizeConfig {
     screenHeight = _mediaQueryData.size.height;
     blockSizeHorizontal = screenWidth / 100;
     blockSizeVertical = screenHeight / 100;
-
-    _safeAreaHorizontal =
-        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
-    _safeAreaVertical =
-        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
-    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
-    safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
 
     h1 = blockSizeHorizontal * 4.0;
     h2 = blockSizeHorizontal * 3.7;
